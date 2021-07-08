3ce49e6f63a0e852f3ccc2433c4d1b80659da655
aaronmdenny <aaronmdenny@outlook.com>
2021-07-08 18:04:12 -0400

    navigating programatically

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  // Inject the router so we can control navigation.
+  constructor(private router: Router) { }
 
   ngOnInit() {
   }
 
+  onLoadServers(): void {
+    // complex calculation
+    // ...done! Now we want to navigate away.
+
+    /**
+     * The navigate() method takes an argument that allows us to navigate to a new route. The route is defined as an
+     * array of the single or the different elements of the new path. The first element in the array is the first part
+     * of the path. If it begins with the / character, the path is absolute. Relative paths are also possible. Just keep
+     * in mind what the path is relative to.
+     */
+    this.router.navigate(['/servers'])
+  }
 }
