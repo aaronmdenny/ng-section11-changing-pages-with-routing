7b5e49780a9a06d73cd031e70108de774120c7c0
aaronmdenny <aaronmdenny@outlook.com>
2021-07-08 12:16:28 -0400

    setting up and loading routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -11,6 +11,42 @@ import { UserComponent } from './users/user/user.component';
 import { EditServerComponent } from './servers/edit-server/edit-server.component';
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
+
+/**
+ * We set up our routes here, in AppModule.
+ * 
+ * Each route is an element of the appRoutes array. Each route is a JavaScript object in the array.
+ * 
+ * Route configuration follows a specific pattern. It always needs a path. The path is a string. So, you can visit the
+ * 'users' path by navigating to:
+ * 
+ *      <yourDomain>/users
+ * 
+ * We do not add the leading / to the path. So do not make the path value '/users'.
+ * 
+ * We also define what should happen when this path is reached. This is typically a component. You inform Angular which
+ * component should be loaded on this path.
+ * 
+ * The empty path '' should take us to the HomeComponent in this example app. Although in general you don't need an
+ * empty path, you will need to handle the situation where the user visits your domain with no path specified. This is
+ * covered later.
+ * 
+ * 
+ */
+const appRoutes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'users',
+    component: UsersComponent
+  },
+  {
+    path: 'servers',
+    component: ServersComponent
+  }
+]
 
 @NgModule({
   declarations: [
@@ -22,9 +58,14 @@ import { ServersService } from './servers/servers.service';
     EditServerComponent,
     ServerComponent
   ],
+  /**
+   * Add RouterModule to the imports array to give the app routing functionality. forRoot() allows us to register
+   * routes.
+   */
   imports: [
     BrowserModule,
     FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [ServersService],
   bootstrap: [AppComponent]
